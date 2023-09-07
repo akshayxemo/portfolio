7a6dbce0d8c19470553e6481d5e14067d5ff0d71
akshayxemo <akshaykrdas001@gmail.com>
2023-09-07 14:39:23 +0530

    about sec dynamic data & cleanup, education added

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import { BiSolidQuoteLeft } from "react-icons/bi";
-import {
-  FcAbout,
-  FcBriefcase,
-  FcGraduationCap,
-  //   FcRatings,
-} from "react-icons/fc";
+import { FcAbout, FcBriefcase, FcGraduationCap } from "react-icons/fc";
+import Education from "./Education";
+import Edu from "../assets/data/education.json";
+import AboutText from "../assets/data/about.json";
 
 export default function About() {
   const [active, setActive] = useState("about");
@@ -13,12 +11,12 @@ export default function About() {
   return (
     <>
       <div className="flex max-sm:flex-col max-w-7xl h-auto mx-auto px-6 max-sm:px-4 pb-20 pt-10 max-sm:pt-0 max-sm:gap-8">
-        <div className="dark:text-white text-dark flex flex-col flex-wrap gap-1 w-[40%] max-sm:w-full max-sm:flex-row max-sm:text-sm max-sm:justify-between max-xsm:justify-center">
+        <div className="dark:text-white text-dark flex flex-col flex-wrap gap-1 w-[40%] max-sm:w-full max-sm:flex-row max-sm:text-sm max-sm:justify-between max-xsm:justify-center font-sans">
           <div
             id="about"
             className={`p-4 cursor-pointer rounded-md hover:bg-slate-500/5 flex max-sm:flex-col items-center gap-4 ${
               active === "about"
-                ? "bg-slate-500/5 border-l-4 border-sky-500 dark:bg-slate-500/10"
+                ? "bg-slate-500/5 border-l-4 max-sm:border-l-0 max-sm:border-b-4 border-sky-500 dark:bg-slate-500/10"
                 : "border-b border-slate-500/10"
             }`}
             onClick={() => setActive("about")}
@@ -30,7 +28,7 @@ export default function About() {
             id="education"
             className={`p-4 cursor-pointer rounded-md hover:bg-slate-500/5 flex max-sm:flex-col items-center gap-4 ${
               active === "education"
-                ? "bg-slate-500/5 border-l-4 border-sky-500 dark:bg-slate-500/10"
+                ? "bg-slate-500/5 border-l-4 max-sm:border-l-0 max-sm:border-b-4 border-sky-500 dark:bg-slate-500/10"
                 : "border-b border-slate-500/10"
             }`}
             onClick={() => setActive("education")}
@@ -38,23 +36,11 @@ export default function About() {
             <FcGraduationCap className="text-3xl max-xsm:text-xl" />
             Education
           </div>
-          {/* <div
-            id="internship"
-            className={`p-4 cursor-pointer rounded-md hover:bg-slate-500/5 flex max-sm:flex-col items-center gap-4 ${
-              active === "internship"
-                ? "bg-slate-500/5 border-l-4 border-sky-500 dark:bg-slate-500/10"
-                : "border-b border-slate-500/10"
-            }`}
-            onClick={() => setActive("internship")}
-          >
-            <FcBriefcase className="text-3xl max-xsm:text-xl" />
-            Internships
-          </div> */}
           <div
             d="experience"
             className={`p-4 cursor-pointer rounded-md hover:bg-slate-500/5 flex max-sm:flex-col items-center gap-4 ${
               active === "experience"
-                ? "bg-slate-500/5 border-l-4 border-sky-500 dark:bg-slate-500/10"
+                ? "bg-slate-500/5 border-l-4 max-sm:border-l-0 max-sm:border-b-4 border-sky-500 dark:bg-slate-500/10"
                 : "border-b border-slate-500/10"
             }`}
             onClick={() => setActive("experience")}
@@ -64,17 +50,31 @@ export default function About() {
             Experience
           </div>
         </div>
-        <div className="w-[60%] max-sm:w-full px-6 py-4">
-          <div className="dark:text-white text-black text-center flex flex-col gap-2 items-center px-8 max-xxsm:px-2">
-            <BiSolidQuoteLeft className="text-8xl max-sm:text-5xl opacity-10" />
-            <p className="leading-[30px] text-xl max-sm:text-lg max-xxsm:text-sm max-xxsm:leading-6">
-              {`Hello, I'm Akshay Kumar Das, a joyful, cheerful, and enthusiastic
-              individual with a passion for creating and learning. I thrive on
-              building innovative solutions and exploring new technologies. My
-              love for creativity extends to graphic design, where I enjoy
-              crafting visually appealing and impactful designs.`}
-            </p>
-          </div>
+        <div className="w-[60%] h-[30rem] overflow-y-auto max-sm:w-full px-6 py-4">
+          {active === "about" ? (
+            <div className="dark:text-white text-black text-center flex flex-col gap-2 items-center px-8 max-xxsm:px-2">
+              <BiSolidQuoteLeft className="text-8xl max-sm:text-5xl dark:opacity-20 opacity-10" />
+              <p className="leading-[30px] text-xl max-sm:text-lg max-xxsm:text-sm max-xxsm:leading-6">
+                {AboutText.about}
+              </p>
+            </div>
+          ) : active === "education" ? (
+            Edu.map((item) => {
+              return (
+                <Education
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  from={item.from}
+                  year={item.year}
+                  marks={item.marks}
+                  status={item.status}
+                />
+              );
+            })
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </>
---
+++ client/src/components/Education.jsx
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types";
+
+Education.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  from: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  marks: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+};
+
+export default function Education({ id, title, from, year, marks, status }) {
+  return (
+    <>
+      <div
+        id={id}
+        className="dark:text-white mb-4 last:mb-0 text-black p-6 dark:bg-slate-500/10 bg-slate-500/5 rounded-lg"
+      >
+        <div className="flex flex-row justify-between max-xsm:flex-wrap gap-3 mb-3">
+          <div>
+            <h1 className="text-2xl break-words mb-1 font-semibold font-sans">
+              {title}
+            </h1>
+            <p className="break-words mb-4">{from}</p>
+            {status === "Pursuing" ? (
+              <span className="rounded-full py-1 px-4 bg-green-500/40 dark:text-green-400 text-green-700">
+                {status}
+              </span>
+            ) : (
+              <span className="rounded-full py-1 px-4 bg-sky-500/40 dark:text-sky-400 text-sky-700">
+                {status}
+              </span>
+            )}
+          </div>
+          <div>
+            <span className="text-sm max-xxsm:text-xs max-xxsm:leading-[0.5rem] font-light italic text-slate-500">
+              {year}
+            </span>
+          </div>
+        </div>
+        <hr className="my-4 dark:border-gray-400/30 border-gray-700/20"></hr>
+        <div>
+          <p className="text-slate-500">{marks}</p>
+        </div>
+      </div>
+    </>
+  );
+}
