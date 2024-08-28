450ddce30a262b4d80eb279bad7375808ae49b2a
akshayxemo <akshaykrdas001@gmail.com>
2024-08-28 21:45:30 +0530

    project card modifications

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -44,7 +44,7 @@ export default function ProjectCard({
   }
   return (
     <>
-      <div className="max-w-[100%]" id={id} onClick={openModal}>
+      <div className="max-w-[100%]" id={id}>
         <CardContainer className="inter-var">
           <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.3] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto max-w-lg h-auto rounded-xl p-6 border  ">
             <CardItem
@@ -66,7 +66,7 @@ export default function ProjectCard({
                   {`${description.slice(0, 99)} ... `}
                   <span
                     className="cursor-pointer text-cyan-500 hover:text-cyan-300"
-                    // onClick={openModal}
+                    onClick={openModal}
                   >
                     Read more.
                   </span>
@@ -98,23 +98,33 @@ export default function ProjectCard({
               {format(parseISO(updatedAt), "MMM d, yyyy")}
             </CardItem>
             <div className="flex justify-between items-center mt-4">
-              <CardItem
-                translateZ={20}
-                as="a"
-                // onClick={()=>{}}
-                href={htmlLink}
-                target="__blank"
-                className="px-6 py-3 rounded-md bg-black dark:bg-white dark:text-black text-white text-sm font-bold flex gap-3 cursor-pointer"
-              >
-                <FaGithub /> Github
-              </CardItem>
+              <div className="flex flex-1 justify-start items-center gap-2">
+                <CardItem
+                  translateZ={20}
+                  as="a"
+                  // onClick={()=>{}}
+                  href={htmlLink}
+                  target="__blank"
+                  className="px-6 py-3 rounded-md bg-black dark:bg-white dark:text-black text-white text-sm font-semibold flex gap-3 cursor-pointer"
+                >
+                  <FaGithub /> Github
+                </CardItem>
+                <button
+                  // translateZ={20}
+                  // as="button"
+                  onClick={openModal}
+                  className="px-6 py-3 rounded-md bg-white dark:bg-black dark:text-white text-black text-sm font-normal flex gap-3 border-[0.2px] dark:border-gray-600 border-gray-500 cursor-pointer hover:bg-black/10 dark:hover:bg-white/10"
+                >
+                  View details
+                </button>
+              </div>
               {homepage && (
                 <CardItem
                   translateZ={20}
                   as="a"
                   href={homepage}
                   target="__blank"
-                  className="px-4 py-2 rounded-xl text-sm font-normal dark:text-white"
+                  className="px-4 py-2 rounded-xl text-sm font-normal dark:text-white dark:hover:text-cyan-500"
                 >
                   Check now →
                 </CardItem>
@@ -143,37 +153,59 @@ export default function ProjectCard({
                 className="text-2xl hover:text-white text-slate-500 cursor-pointer"
               />
             </div>
-            <p className="mt-3">{description}</p>
-            <div
-              height="1000"
-              width="1000"
-              className="w-full mt-4 bg-cover overflow-hidden aspect-video bg-black relative rounded-xl dark:border-white/[0.1] border"
-            >
-              <img
-                src={preview == "" ? demoImg : preview}
+            <div className="overflow-y-auto mt-3">
+              <p className="">{description}</p>
+              <div
                 height="1000"
                 width="1000"
-                className="group-hover/card:shadow-xl"
-                alt="thumbnail"
-              />
-            </div>
-            <div className="text-neutral-500 text-sm dark:text-neutral-300 w-full mt-5">
-              {tags.length ? (
-                <div className="flex flex-wrap gap-3 items-center">
-                  {tags.map((tag, index) => {
-                    return (
-                      <span
-                        className="rounded-full py-1 px-4 border"
-                        key={index}
-                      >
-                        {tag}
-                      </span>
-                    );
-                  })}
+                className="w-full mt-4 bg-cover overflow-hidden aspect-video bg-black relative rounded-xl dark:border-white/[0.1] border"
+              >
+                <img
+                  src={preview == "" ? demoImg : preview}
+                  height="1000"
+                  width="1000"
+                  className="group-hover/card:shadow-xl"
+                  alt="thumbnail"
+                />
+              </div>
+              <div className="text-neutral-500 text-sm dark:text-neutral-300 w-full mt-5">
+                {tags.length ? (
+                  <div className="flex flex-wrap gap-3 items-center">
+                    {tags.map((tag, index) => {
+                      return (
+                        <span
+                          className="rounded-full py-1 px-4 border"
+                          key={index}
+                        >
+                          {tag}
+                        </span>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="flex justify-between items-center mt-6">
+                <div className="flex flex-1 justify-start items-center gap-2">
+                  <a
+                    href={htmlLink}
+                    target="__blank"
+                    className="px-6 py-3 rounded-md bg-black dark:bg-white dark:text-black text-white text-sm font-semibold flex gap-3 cursor-pointer"
+                  >
+                    <FaGithub /> Github
+                  </a>
                 </div>
-              ) : (
-                ""
-              )}
+                {homepage && (
+                  <a
+                    href={homepage}
+                    target="__blank"
+                    className="px-4 py-2 rounded-xl text-sm font-normal dark:text-white dark:hover:text-cyan-500"
+                  >
+                    Check now →
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         </Zoom>
